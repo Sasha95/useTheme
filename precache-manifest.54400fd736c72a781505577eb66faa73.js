70a313f6ae88bc563e434f00456dbba43fd2006b
Sasha <sasha982025@gmail.com>
2019-08-29 02:17:00 +0300

    Updates

---
+++ precache-manifest.54400fd736c72a781505577eb66faa73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb721c7549f2649ae400deb3c62d3930",
+    "url": "/useTheme/index.html"
+  },
+  {
+    "revision": "1c2eafdba0e0a5dc75dc",
+    "url": "/useTheme/static/css/main.a6300bcc.chunk.css"
+  },
+  {
+    "revision": "5ef4d7f298ef17a21df2",
+    "url": "/useTheme/static/js/2.257671ae.chunk.js"
+  },
+  {
+    "revision": "1c2eafdba0e0a5dc75dc",
+    "url": "/useTheme/static/js/main.e41f794f.chunk.js"
+  },
+  {
+    "revision": "f368476bf097eedf0bdc",
+    "url": "/useTheme/static/js/runtime~main.1cafb4d3.js"
+  }
+]);
